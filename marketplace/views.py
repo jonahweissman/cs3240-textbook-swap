684bbabe363f40cd166630c3e0d8ca203c291bfa
np6hd <np6hd@virginia.edu>
2020-02-14 16:48:14 -0500

    Added Home

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
+
 
 # Create your views here.
-def index(request):
-    context = {
-        'user': request.user
-    }
-    return render(request, 'marketplace/main.html', context)
+class IndexViews(generic.DetailView):
+    template_name = "marketplace/main.html"
+
+    def get(self, request):
+        return render(request, self.template_name, {
+            'user': request.user
+        })
