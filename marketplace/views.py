2dfda30150fce4c64a0a704fb66b8335562e2aa2
Jonah Weissman <jw5av@virginia.edu>
2020-02-14 15:40:48 -0500

    Fix home page for users who aren't logged in

--- marketplace/views.py
+++ marketplace/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     context = {
-        'name': request.user.first_name
+        'user': request.user
     }
     return render(request, 'marketplace/main.html', context)
