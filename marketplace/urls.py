fd81badbb3d28b2129dbe3a01d5e076d52b4ba45
Jonah Weissman <jw5av@virginia.edu>
2020-02-13 21:00:57 -0500

    Add Google OAuth2 with social_auth
    https://python-social-auth.readthedocs.io/en/latest/configuration/settings.html

---
+++ marketplace/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+
+from . import views
+
+urlpatterns = [
+    path('', include('social_django.urls', namespace='social')),
+    path('', views.index, name='index'),
+]
