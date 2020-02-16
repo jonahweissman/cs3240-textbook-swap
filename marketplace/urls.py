312d6eed562d3fad26f0dbd349b201b1281e148d
Ashish Upadhyaya <ashishsubedi28@gmail.com>
2020-02-16 00:01:37 -0500

    Added Templates

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -2,7 +2,9 @@ from django.urls import path, include
 
 from . import views
 
+#app_name = 'marketplace'
 urlpatterns = [
     path('', include('social_django.urls', namespace='social')),
     path('', views.IndexViews.as_view(), name='index'),
+    path('addListing', views.ListingViews.as_view(), name = 'addListing'),
 ]
