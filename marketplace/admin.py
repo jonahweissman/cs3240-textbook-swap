d7e0b33590d13a9825ec237004fdc006b6d85c93
Ashish Upadhyaya <ashishsubedi28@gmail.com>
2020-02-21 18:43:03 -0500

    New model with user profile auto created with login

--- marketplace/admin.py
+++ marketplace/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import User, Item
+from .models import Profile, Item
 # Register your models here.
 
-admin.site.register(User)
+admin.site.register(Profile)
 admin.site.register(Item)
