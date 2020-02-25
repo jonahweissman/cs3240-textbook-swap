4b3526a5ee9792fb588168b6958357d6dde83e9f
Jonah Weissman <jw5av@virginia.edu>
2020-02-25 17:25:13 -0500

    Remove unnecessary .env code

--- textbookswap/settings.py
+++ textbookswap/settings.py
@@ -18,9 +18,8 @@ import dotenv
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-dotenv_file = os.path.join(BASE_DIR, ".env")
-if os.path.isfile(dotenv_file):
-    dotenv.load_dotenv(dotenv_file)
+# Load environment variables from .env file
+dotenv.load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
