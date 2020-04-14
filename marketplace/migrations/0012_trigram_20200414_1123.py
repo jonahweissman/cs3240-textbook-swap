4172108d14221f554c0b916a9d97756f0921699b
matt2newberry <60236456+matt2newberry@users.noreply.github.com>
2020-04-14 17:20:01 -0400

    Added dependency

--- marketplace/migrations/0012_trigram_20200414_1123.py
+++ marketplace/migrations/0012_trigram_20200414_1123.py
@@ -6,6 +6,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('marketplace', '0011_merge_20200410_1308'),
+        ('marketplace', '0012_auto_20200414_1644')
     ]
 
     operations = [
