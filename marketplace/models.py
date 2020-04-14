d69df7bfaab26f6c51a5e36a185dc3307643b1e6
matt2newberry <60236456+matt2newberry@users.noreply.github.com>
2020-04-14 19:37:57 -0400

    fixed merge mistake

--- marketplace/models.py
+++ marketplace/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from phone_field import PhoneField
 from django_auto_one_to_one import AutoOneToOneModel
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.urls import reverse
+from django.core.validators import MinValueValidator
 import uuid
 import os
 # Create your models here.
@@ -11,10 +13,10 @@ import os
 class Profile(AutoOneToOneModel(User)):
     
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    imagefile = models.ImageField(upload_to='images/', null=True)
-    phonenumber = models.CharField(max_length=12, null=True)
-    major = models.CharField(max_length=50, null=True)
-    year = models.CharField(max_length=4, null=True)
+    imagefile = models.ImageField(upload_to='images/', null=True, blank=True)
+    phonenumber = PhoneField(null=True, blank=True)
+    major = models.CharField(max_length=50, null=True, blank=True)
+    year = models.IntegerField(null=True, blank=True, validators=[MinValueValidator(0)])
 
     def __str__(self):
         return f'{self.user.first_name} {self.user.last_name}'
