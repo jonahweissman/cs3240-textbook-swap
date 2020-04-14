a8c80b7ce9aa4b87d40fe48fcf16f181e31720f2
matt2newberry <60236456+matt2newberry@users.noreply.github.com>
2020-04-14 18:46:10 -0400

    fixing merge conflicts

--- marketplace/models.py
+++ marketplace/models.py
@@ -1,11 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
-from phone_field import PhoneField
 from django_auto_one_to_one import AutoOneToOneModel
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.urls import reverse
-from django.core.validators import MinValueValidator
 import uuid
 import os
 # Create your models here.
@@ -13,10 +11,10 @@ import os
 class Profile(AutoOneToOneModel(User)):
     
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    imagefile = models.ImageField(upload_to='images/', null=True, blank=True)
-    phonenumber = PhoneField(null=True, blank=True)
-    major = models.CharField(max_length=50, null=True, blank=True)
-    year = models.IntegerField(null=True, blank=True, validators=[MinValueValidator(0)])
+    imagefile = models.ImageField(upload_to='images/', null=True)
+    phonenumber = models.CharField(max_length=12, null=True)
+    major = models.CharField(max_length=50, null=True)
+    year = models.CharField(max_length=4, null=True)
 
     def __str__(self):
         return f'{self.user.first_name} {self.user.last_name}'
@@ -57,7 +55,7 @@ class Item(models.Model):
     item_description = models.TextField(max_length= 1000, null=True)
     item_status_choices = (("Available", "Available"),("Sold", "Sold"), ("Unavailable", "Unavailable"))
     item_status = models.CharField(max_length=20, choices=item_status_choices, default= "Available")
-    count = User.objects.count()
+
 
     def __str__(self):
         return self.item_name
@@ -78,7 +76,7 @@ class Message(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     text = models.TextField()
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    in_response_to = models.ForeignKey('self', on_delete=models.CASCADE, null=True)
+    in_response_to = models.OneToOneField('self', on_delete=models.CASCADE, null=True)
     conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE)
 
     def __str__(self):
--- marketplace/views.py
+++ marketplace/views.py
@@ -11,7 +11,10 @@ from django.db.models import Q
 from .models import Item, Profile
 from django.shortcuts import get_object_or_404
 from django.contrib import messages
+from django.contrib.postgres.search import SearchVector, SearchQuery, TrigramDistance
+
 import requests
+
 
 
 # Create your views here.
@@ -95,7 +98,6 @@ class ProfileViews(generic.DetailView):
 
     def get(self, request):
         Profiles = Profile.objects.all()
-
         return render(request, self.template_name, {
             'user': request.user,
     })
@@ -112,7 +114,6 @@ class EditProfileViews(generic.DetailView):
     template_name = "marketplace/edit_profile.html"
 
     def get(self, request):
-
         form = ImageForm()
         return render(request, self.template_name, {
             'user': request.user, "form": form
@@ -126,7 +127,6 @@ class EditProfileViews(generic.DetailView):
             form = ImageForm()
         args = {"form": form}
         return render(request, self.template_name, args)
-
 
 class MyListings(generic.ListView):
     template_name = "marketplace/myListings.html"
@@ -155,14 +155,20 @@ class SearchViews(generic.ListView):
     context_object_name = 'search_results'
     paginate_by = 10
     sort_mapping = {
-        'date': '-item_posted_date',
-        'price': 'item_price',
+        'relevance': [
+            'name_distance',
+            'author_distance',
+            'description_distance',
+        ],
+        'date': ['-item_posted_date'],
+        'price': ['item_price'],
     }
+    sort_default = 'relevance'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['query'] = self.request.GET['query']
-        context['sort'] = self.request.GET.get('sort', 'date')
+        context['sort'] = self.request.GET.get('sort', self.sort_default)
         page = context['page_obj']
         context['next_page'] = page.next_page_number() if page.has_next() else None
         context['previous_page'] = page.previous_page_number() if page.has_previous() else None
@@ -171,12 +177,17 @@ class SearchViews(generic.ListView):
 
     def get_queryset(self):
         query = self.request.GET['query']
-        sort_by = self.request.GET.get('sort', 'date')
+        sort_by = self.request.GET.get('sort', self.sort_default)
         order_by = self.sort_mapping[sort_by]
-        return self.model.objects.all().filter(
-            Q(item_name__icontains=query)
-            | Q(item_description__icontains=query)
-        ).order_by(order_by)
+        hit_filter = Q(name_distance__lte=0.8) \
+                     | Q(description_distance__lte=0.7) \
+                     | Q(author_distance__lte=0.7) \
+                     | Q(item_isbn=query)
+        return self.model.objects.annotate(
+            name_distance=TrigramDistance('item_name', query),
+            description_distance=TrigramDistance('item_description', query),
+            author_distance=TrigramDistance('item_author', query),
+        ).filter(hit_filter).order_by(*order_by)
 
 class ItemDetail(generic.DetailView):
     model=Item
